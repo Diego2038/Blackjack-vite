351ac69aceb5911edc43044c070fc0035f5c23dd
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2023-01-29 21:05:56 -0500

    implementado JSDoc para documentar una función en JS (por eso es muchísimo mejor TS)

--- src/black-jack/usecases/crear-deck.js
+++ src/black-jack/usecases/crear-deck.js
@@ -3,8 +3,22 @@ import _ from 'underscore'
 
 export const miNombre = 'Juan Diego';
 
+// ! Advertencia
+// TODO: para definir mejor lo que tengo que hacer
+// ? Pregunta
+// * Importante
+
+ 
+/**
+ * Función que se encarga de retornar un mazo aleatorio de cartas
+ * @param { Array<String>} tiposDeCarta Ejemplo: ['C','D','H','S']
+ * @param { Array<String>} tiposEspeciales Ejemplo: ['A','J','Q','K']
+ * @returns { Array<String> } retorna un mazo de cartas
+ */
 const crearDeck = ( tiposDeCarta, tiposEspeciales) => {
 
+  if( !tiposDeCarta || tiposDeCarta.length === 0 ) throw 'tiposDeCarta debe de ser un arreglo con elementos';
+  if( !tiposEspeciales || tiposEspeciales.length === 0 ) throw 'tiposEspeciales debe de ser un arreglo con elementos';
   let deck = [];
 
   for( let i = 2; i <= 10; i++ ) {
